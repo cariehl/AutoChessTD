5859cf314587d661d09cb91807b13ad6b0f9c8f5
Robert Picking <robertepicking@gmail.com>
2019-06-24 21:03:32 -0400

    Tower capital now always looks at its target

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace AutoChessTD {
+
     public class Tower : MonoBehaviour {
 
-        // Start is called before the first frame update
-        void Start() {
+        [SerializeField] private float ROTATION_SPEED = 10f;
 
+        public Transform capital;
+
+        [SerializeField] private GameObject _target;
+        public GameObject Target {
+            get { return _target; }
+            private set {
+                _target = value;
+            }
         }
 
-        // Update is called once per frame
-        void Update() {
+        private void Start() {
+            
+        }
 
+        private void Update() {
+            LookAtTarget();
+        }
+
+        private void LookAtTarget() {
+            if (Target == null) return;
+
+            Vector3 diff = Target.transform.position - capital.position;
+            capital.rotation = Quaternion.Slerp(capital.rotation, Quaternion.LookRotation(diff), ROTATION_SPEED * Time.deltaTime);
         }
     }
 }
