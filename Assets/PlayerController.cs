9943729506fc190be536426ea1c64fd54cfb5678
Cody Wilson <mail.codyw@gmail.com>
2019-08-27 21:09:08 -0400

    created a scene with minion navigating to home around tower.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent.SetDestination(new Vector3(12, 1, 2));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
